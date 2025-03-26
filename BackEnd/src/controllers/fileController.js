24e90c739680ecd58ce7e910ed96fe7ca18acc7a
Gustavohaa <gustavohaa7@gmail.com>
2025-03-26 00:14:14 -0300

    feat: create controller for file download

--- BackEnd/src/controllers/fileController.js
+++ BackEnd/src/controllers/fileController.js
@@ -1,1 +1,15 @@
+const fileService = require("../services/fileService");
 
+const downloadFile = (req, res) => {
+  const filePath = fileService.getFilePath();
+
+  res.download(filePath, "Hello.txt", (err) => {
+    if (err) {
+      if (!res.headersSent) {
+        res.status(500).json({ message: "Erro ao baixar o arquivo." });
+      }
+    }
+  });
+};
+
+module.exports = { downloadFile };
